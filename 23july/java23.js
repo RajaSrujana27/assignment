e148895c0c7270e36358c6e98003d97bbf479f87
RajaSrujana27 <adapasrujana27@gmail.com>
2024-07-24 18:46:32 +0530

    tasks

---
+++ 23july/java23.js
@@ -0,0 +1,66 @@
+// Initialize an object with nested properties
+const originalObject = {
+    name: 'Srujana',
+    address: {
+      city: 'Allavaram',
+      zip: '12345'
+    }
+};
+  const shallowCopy1 = Object.assign({}, originalObject);
+  const shallowCopy2 = { ...originalObject };
+  
+  shallowCopy1.address.city = 'Amalapuram';
+  shallowCopy2.address.zip = '54321';
+  console.log('Original Object:', originalObject);
+  console.log('Shallow Copy using Object.assign:', shallowCopy1);
+  console.log('Shallow Copy using spread operator:', shallowCopy2);
+  
+  // Initialize an object with nested properties
+const originalObj2 = {
+    name: 'sai',
+    address: {
+      city: 'Nizampet',
+      zip: '94101'
+    }
+  };
+  const deepCopy = JSON.parse(JSON.stringify(originalObj2));
+  
+  deepCopy.address.city = 'chintal';
+  deepCopy.address.zip = '98101';
+  console.log('Original Object:', originalObj2);
+  console.log('Deep Copy:', deepCopy);
+
+  // Initialize two arrays
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let combinedArray = [...array1, ...array2];
+console.log(combinedArray);
+
+// Initialize two objects
+let object1 = {a: 1, b: 2};
+let object2 = {c: 3, d: 4};
+let combinedObject = {...object1, ...object2};
+console.log(combinedObject);
+
+// Function that sums any number of arguments
+function sum(...numbers) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
+}
+console.log(sum(1, 2, 3)); 
+console.log(sum(4, 5, 6, 7)); 
+console.log(sum(8, 9)); 
+
+// Initialize an array with at least five elements
+let array = [10, 20, 30, 40, 50];
+let [first, second, ...rest] = array;
+console.log(first); 
+console.log(second); 
+console.log(rest); 
+
+
+
+
+
+
+
+  
---
+++ 24july/24july.js
@@ -0,0 +1,60 @@
+// 1. Using forEach
+const numbers = [1, 2, 3, 4, 5];
+
+function printNumbers(array) {
+  array.forEach(number => {
+    console.log(number);
+  });
+}
+
+printNumbers(numbers);
+
+// 2. Using map
+const numbers2 = [1, 2, 3, 4, 5];
+
+function doubleNumbers(array) {
+  const doubledNumbers = array.map(number => number * 2);
+  console.log(doubledNumbers);
+}
+
+doubleNumbers(numbers2);
+
+// 3. Using filter
+function filterEvenNumbers(numbers) {
+    return numbers.filter(number => number % 2 === 0);
+}
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = filterEvenNumbers(numbersArray);
+console.log('Even numbers:', evenNumbers);
+
+// 4. Using reduce
+function sumOfNumbers(numbers) {
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+const sum = sumOfNumbers(numbersArray);
+console.log('Sum of numbers:', sum);
+
+// 5. Using find
+const people = [
+    { name: 'Srujana', age: 20 },
+    { name: 'praneeth', age: 25 },
+    { name: 'Alekhya', age: 40 },
+    { name: 'chandu', age: 35 }
+];
+
+function findPersonOlderThan25(people) {
+    return people.find(person => person.age > 25);
+}
+
+const personOlderThan25 = findPersonOlderThan25(people);
+console.log('First person older than 25:', personOlderThan25 ? personOlderThan25.name : 'None');
+
+// 6. Using findIndex
+function findIndexYoungerThan18(people) {
+    return people.findIndex(person => person.age < 18);
+}
+
+const indexYoungerThan18 = findIndexYoungerThan18(people);
+console.log('Index of first person younger than 18:', indexYoungerThan18);
